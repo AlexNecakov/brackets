cb92672d67ab96f268ea73d66e5e3f12684c4692
Alex Necakov <alexnecakov@gmail.com>
2024-11-18 19:02:11 -0500

    fixed btree to go syntax

--- go/main.go
+++ go/main.go
@@ -25,13 +25,13 @@ func createNode(parent *node) node {
 }
 
 func main() {
-	numEntries := float64(len(os.Args[1:]))
+	numEntries := len(os.Args[1:])
 	if numEntries <= 0 {
-		fmt.Println("no entries provided\n")
+		fmt.Println("no entries provided")
 		os.Exit(1)
 	}
-	numRounds := math.Ceil(math.Log2(numEntries))
-	numLeaves := math.Ceil(math.Pow(2, numRounds))
+	numRounds := int(math.Ceil(math.Log2(float64(numEntries))))
+	numLeaves := int(math.Ceil(math.Pow(2, float64(numRounds))))
 
 	rootNode := createNode(nil)
 	current := &rootNode
@@ -39,45 +39,40 @@ func main() {
 	currLeaves := 0
 	currDepth := 0
 
-    // make the btree
-    while currLeaves < numLeaves {
-        // leaf
-        if currDepth == numRounds {
-            if currLeaves < numEntries {
-                setNamecurrent, argv[1 + currLeaves]
-                printf"set leaf %i name to %s\n", currLeaves, current->name
-            }
-            currLeaves++
-            current->decided = 1
-            current = current->parent
-            currDepth--
-        }
-        // too deep
-        else if currDepth > numRounds {
-            printf"delved too deep. crashing\n"
-            return 1
-        } else if currDepth < 0 {
-            printf"flew too close to the sun. crashing\n"
-            return 1
-        }
-        // non leaf
-        else {
-            if current->child_left == NULL {
-                node *newNode = createNodecurrent
-                current->child_left = newNode
-                current = newNode
-                currDepth++
-            } else if current->child_right == NULL {
-                node *newNode = createNodecurrent
-                current->child_right = newNode
-                current = newNode
-                currDepth++
-            } else {
-                current = current->parent
-                currDepth--
-            }
-        }
-    }
-
+	// make the btree
+	for currLeaves < numLeaves {
+		// leaf
+		if currDepth == numRounds {
+			if currLeaves < numEntries {
+				current.name = string(os.Args[1+currLeaves])
+				fmt.Printf("set leaf %d name to %s\n", currLeaves, current.name)
+			}
+			currLeaves++
+			current.decided = true
+			current = current.parent
+			currDepth--
+		} else if currDepth > numRounds {
+			fmt.Println("delved too deep. crashing")
+			os.Exit(1)
+		} else if currDepth < 0 {
+			fmt.Println("flew too close to the sun. crashing")
+			os.Exit(1)
+		} else {
+			if current.child_left == nil {
+				newNode := createNode(current)
+				current.child_left = &newNode
+				current = &newNode
+				currDepth++
+			} else if current.child_right == nil {
+				newNode := createNode(current)
+				current.child_right = &newNode
+				current = &newNode
+				currDepth++
+			} else {
+				current = current.parent
+				currDepth--
+			}
+		}
+	}
 
 }
